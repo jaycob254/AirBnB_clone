f68afb6212792d5054202145e0728a416c777c97
Stanley <jaycobstano@gmail.com>
2022-10-27 02:30:15 -0700

    BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -2,15 +2,23 @@
 import uuid
 from datetime import datetime
 
-"""BaseModel that defines all common attributes/methods"""
+"""Base Model"""
 
 
 class BaseModel:
     """BaseModel Class Details"""
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                if key == "__class__":
+                    continue
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Prints in the form: [<class name>] (<self.id>) <self.__dict__>"""
@@ -20,6 +28,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time"""
         self.updated_at = datetime.now()
+        return
 
     def to_dict(self):
         """Return a dictionary containing all keys/values of the instance"""
@@ -27,3 +36,4 @@ class BaseModel:
         new_dictionary.update({'__class__': str(self.__class__.__name__)})
         new_dictionary["created_at"] = self.created_at.isoformat()
         new_dictionary["updated_at"] = self.updated_at.isoformat()
+        return new_dictionary
